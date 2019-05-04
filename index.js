541c2c786654d7f3ba6cff6b1298fae9b554db22
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2019-05-04 19:01:09 +0900

    しゅうせい２

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 
-const server = app.listen(3000, function(){
+const server = app.listen(process.env.port || 3000, function(){
   console.log("Node.js is listening to PORT:" + server.address().port)
 })
 app.use(express.static('saji-firebase/public'));
